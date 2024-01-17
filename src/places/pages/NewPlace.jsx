cd598f48a2ffd26d7c3d2907ddbd92570662c960
Yann Delaqueze <yann.delaqueze@gmail.com>
2024-01-17 16:12:41 +0100

    init commit

---
+++ src/places/pages/NewPlace.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function NewPlace() {
+  return <div>COUCOU</div>;
+}
---
+++ src/user/pages/User.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Users() {
+  return (
+    <>
+      <h2> Users is working !</h2>
+    </>
+  );
+}
