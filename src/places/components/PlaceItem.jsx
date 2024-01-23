18b7e39f7f900d853defd3b59deb6949e94d8781
Yann Delaqueze <yann.delaqueze@gmail.com>
2024-01-23 15:06:00 +0100

    modal wip

--- src/places/components/PlaceItem.jsx
+++ src/places/components/PlaceItem.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import { Card } from "../../shared/components/UIElements/Card";
+import { Button } from "../../shared/components/FormElements/Button";
 
 import "./PlaceItem.css";
 
-export function PlaceItem({ image, title, address, description }) {
+export function PlaceItem({ image, title, address, description, id }) {
   return (
     <li className="place-item">
       <Card className="place-item__content">
@@ -17,9 +18,9 @@ export function PlaceItem({ image, title, address, description }) {
           <p>{description}</p>
         </div>
         <div className="place-item__actions">
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
