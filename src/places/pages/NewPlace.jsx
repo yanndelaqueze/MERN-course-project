0f924655ae983af13b74b5ab36e9e4f5a68196c8
Yann Delaqueze <yann.delaqueze@gmail.com>
2024-01-24 09:56:38 +0100

    new place input form

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+import { Input } from "../../shared/components/FormElements/Input";
+
+import "./NewPlace.css";
+
 export function NewPlace() {
-  return <div>COUCOU</div>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="title"
+        validators={[]}
+        onChange={() => {}}
+      />
+    </form>
+  );
 }
---
+++ src/shared/components/FormElements/Input.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Input.css";
+
+export function Input(props) {
+  const element =
+    props.element === "input" ? (
+      <input id={props.id} type={props.type} placeholder={props.placeholder} />
+    ) : (
+      <textarea id={props.id} row={props.row || 3} />
+    );
+
+  return (
+    <div className={`form-control`}>
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+    </div>
+  );
+}
